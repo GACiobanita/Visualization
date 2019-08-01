e6cdbd7dbe73dd0631523e1474242cf7da1d8d9e
GACiobanita <ciobanita_alex@yahoo.com>
2019-08-01 17:24:45 +0100

    Update __init__.py

--- implementation/__init__.py
+++ implementation/__init__.py
@@ -2,10 +2,12 @@ from .word_cloud_generator import WordCloudGenerator
 from .bar_chart_generator import BarChartGenerator
 from .file_receiver import FileReceiver
 from .data_adjustments import DataAdjustment
+from .sentiment_analyzer import SentimentAnalyzer
 
 __all__= [
             "WordCloudGenerator",
             "BarChartGenerator",
             "FileReceiver",
-            "DataAdjustment"
+            "DataAdjustment",
+            "SentimentAnalyzer"
         ]
