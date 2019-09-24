27a6b12f864c9dbc229f0cff181c22f0cc10feaa
GACiobanita <ciobanita_alex@yahoo.com>
2019-09-24 17:28:34 +0100

    New type of line charts added - Character Distribution Charts

--- main/create_linecharts.py
+++ main/create_linecharts.py
@@ -8,27 +8,35 @@ def main():
     file_receiver.acquire_input_path()
     file_receiver.acquire_output_path()
 
-    lc_generator.acquire_csv_files(file_receiver.csv_files)
-    lc_generator.calculate_overall_reviews_by_identifiable_individuals()
-    lc_generator.calculate_reviews_by_identifiable_individuals_per_app()
-    lc_generator.create_all_year_data_chart()
-    lc_generator.create_yearly_app_data_charts()
+    line_type = input("Yearly lines(Y) or Character Distribution(W): ")
 
-    lc_generator.save_yearly_app_charts(file_receiver.output_folder_path)
-    lc_generator.save_all_year_charts(file_receiver.output_folder_path)
+    if line_type == "Y":
+        lc_generator.acquire_csv_files(file_receiver.csv_files)
+        lc_generator.calculate_overall_reviews_by_identifiable_individuals()
+        lc_generator.calculate_reviews_by_identifiable_individuals_per_app()
+        lc_generator.create_all_year_data_chart()
+        lc_generator.create_yearly_app_data_charts()
 
-    # display line charts
-    while True:
-        input_text = input("Would you like to see the line charts? (Y/N) :")
-        if input_text == 'Y':
-            lc_generator.display_bar_charts()
-            break
-        else:
-            if input_text == 'N':
-                print("Shutting down.")
+        lc_generator.save_yearly_app_charts(file_receiver.output_folder_path)
+        lc_generator.save_all_year_charts(file_receiver.output_folder_path)
+
+        # display line charts
+        while True:
+            input_text = input("Would you like to see the line charts? (Y/N) :")
+            if input_text == 'Y':
+                lc_generator.display_line_charts()
                 break
             else:
-                print("I didn't catch that, try again.")
+                if input_text == 'N':
+                    print("Shutting down.")
+                    break
+                else:
+                    print("I didn't catch that, try again.")
+    elif line_type == "W":
+        lc_generator.acquire_csv_files(file_receiver.csv_files)
+        lc_generator.categorize_text_by_character_count()
+        lc_generator.create_simple_line_chart()
+        lc_generator.save_line_chart(file_receiver.output_folder_path)
 
 
 if __name__ == "__main__":
